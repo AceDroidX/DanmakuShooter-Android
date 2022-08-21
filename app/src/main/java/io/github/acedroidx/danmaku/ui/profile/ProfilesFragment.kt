9c3b42f1ff7f2735abeb222211007732bed8378e
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2022-08-22 05:48:11 +0800

    使用Jetpack Compose

--- app/src/main/java/io/github/acedroidx/danmaku/ui/profile/ProfilesFragment.kt
+++ app/src/main/java/io/github/acedroidx/danmaku/ui/profile/ProfilesFragment.kt
@@ -4,20 +4,30 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import android.view.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import com.google.android.material.switchmaterial.SwitchMaterial
+import dagger.hilt.android.AndroidEntryPoint
 import io.github.acedroidx.danmaku.DanmakuService
 import io.github.acedroidx.danmaku.R
-import io.github.acedroidx.danmaku.databinding.FragmentProfilesBinding
+import io.github.acedroidx.danmaku.ui.theme.AppTheme
 
-
+@AndroidEntryPoint
 class ProfilesFragment : Fragment() {
     private var mService: DanmakuService? = null
     private val connection = object : ServiceConnection {
@@ -28,13 +38,13 @@ class ProfilesFragment : Fragment() {
 
             mService!!.isRunning.observe(viewLifecycleOwner) {
                 Log.d("ProfilesFragment", "isRunning: $it")
-                if (viewModel.isRunning.value != it) {
-                    viewModel.isRunning.value = it
+                if (profilesViewModel.isRunning.value != it) {
+                    profilesViewModel.isRunning.value = it
                 }
                 switchBtn?.isChecked = it
             }
-            viewModel.isRunning.observe(viewLifecycleOwner) {
-                Log.d("ProfilesFragment", "viewModel.isRunning.observe:$it")
+            profilesViewModel.isRunning.observe(viewLifecycleOwner) {
+                Log.d("ProfilesFragment", "profilesViewModel.isRunning.observe:$it")
                 if (mService!!.isRunning.value != it) {
                     mService!!.isRunning.value = it
                     mService!!.isForeground.value = it
@@ -42,7 +52,7 @@ class ProfilesFragment : Fragment() {
                 }
                 switchBtn?.isChecked = it
             }
-            viewModel.isRunning.value = mService!!.isRunning.value
+            profilesViewModel.isRunning.value = mService!!.isRunning.value
         }
 
         override fun onServiceDisconnected(arg0: ComponentName) {
@@ -50,8 +60,7 @@ class ProfilesFragment : Fragment() {
         }
     }
 
-    private val viewModel: ProfilesViewModel by viewModels()
-    private lateinit var binding: FragmentProfilesBinding
+    private val profilesViewModel: ProfilesViewModel by viewModels()
 
     private var switchBtn: SwitchMaterial? = null
 
@@ -64,12 +73,12 @@ class ProfilesFragment : Fragment() {
                 inflater.inflate(R.menu.profiles_action_menu, menu)
                 // https://stackoverflow.com/questions/22918812/add-switch-widget-to-actionbar-and-respond-to-change-event
                 switchBtn =
-                    menu.findItem(R.id.app_bar_switch).actionView.findViewById(R.id.action_switch)
+                    menu.findItem(R.id.app_bar_switch).actionView?.findViewById(R.id.action_switch)
                 switchBtn!!.setOnCheckedChangeListener { button, value ->
-                    viewModel.isRunning.value = value
+                    profilesViewModel.isRunning.value = value
                 }
                 Log.d("ProfilesFragment", "switchBtn")
-                mService?.let { viewModel.isRunning.value = it.isRunning.value }
+                mService?.let { profilesViewModel.isRunning.value = it.isRunning.value }
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
@@ -80,10 +89,34 @@ class ProfilesFragment : Fragment() {
         Intent(context, DanmakuService::class.java).also { intent ->
             activity?.bindService(intent, connection, Context.BIND_AUTO_CREATE)
         }
-        binding = FragmentProfilesBinding.inflate(inflater, container, false)
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
-        return binding.root
+        return ComposeView(requireContext()).apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MyComposable()
+            }
+        }
+    }
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewCompose() {
+        MyComposable()
+    }
+
+    @Composable
+    fun MyComposable(viewModel: ProfilesViewModel = hiltViewModel()) {
+        val uiState = viewModel.text.observeAsState()
+        AppTheme {
+            // In Compose world
+            Text("Hello Compose!", color = MaterialTheme.colorScheme.onBackground)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/io/github/acedroidx/danmaku/ui/profile/ProfilesViewModel.kt
+++ app/src/main/java/io/github/acedroidx/danmaku/ui/profile/ProfilesViewModel.kt
@@ -2,8 +2,11 @@ package io.github.acedroidx.danmaku.ui.profile
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ProfilesViewModel : ViewModel() {
+@HiltViewModel
+class ProfilesViewModel @Inject constructor() : ViewModel() {
     val text = MutableLiveData<String>().apply { value = "This is Profiles Fragment" }
 
     val isRunning = MutableLiveData<Boolean>().apply { value = false }
